f511aefa93e2bb1bd110f0d17c13f4b310de203d
sovranliu <sovranliu@aliyun.com>
2016-08-31 10:45:57 +0800

    梳理首页代码

--- assets/h5/afam/js/controller/page.home.js
+++ assets/h5/afam/js/controller/page.home.js
@@ -5,9 +5,12 @@ $Controller.bind = function() {
 	swipe('.adver-wrap','.adver-pagination');
 	swipe('.lhb-wrap','.lhb-pagination');
 	swipe('.yz-wrap','.yz-pagination');
-	// 加载更多
-	loadMore();
+	initNews();
+	// 加载数据
 	loadBanner();
+	loadTopTransaction();
+	loadTopTransfer();
+	loadNews();
 };
 
 function swipe(wrap,pagination) {
@@ -23,7 +26,7 @@ function swipe(wrap,pagination) {
 function loadBanner() {
 	$$.getJSON(S_DOMAIN + '/afam/rest/banner', {}, function(resp) {
 		if(resp.code < 0) {
-			bridge('window').call('tip', resp.msg || '服务器错误');
+			bridge('window').call('tip', resp.msg || '广告服务错误');
 			return;
 		}
 		var html = Template7.templates.template_banner(resp.data);
@@ -31,50 +34,59 @@ function loadBanner() {
 	});
 }
 
-function loadMore() {
-	// 加载flag
-	var loading = false;
-	// 上次加载的序号
-	var lastIndex = $$('.list-block li').length;
-	// 最多可加载的条目
-	var maxItems = 60;
-	// 每次加载添加多少条目
-	var itemsPerLoad = 20;
-	// 注册'infinite'事件处理函数
+function loadTopTransaction() {
+	$$.getJSON(S_DOMAIN + '/afam/rest/transactionTop', {}, function(resp) {
+		if(resp.code < 0) {
+			bridge('window').call('tip', resp.msg || '交易龙虎榜服务错误');
+			return;
+		}
+		var html = null;
+		html = Template7.templates.template_top_transaction(resp.data.slice(0, 3));
+		$$('#top_transaction_1').html(html);
+		html = Template7.templates.template_top_transaction(resp.data.slice(3, 6));
+		$$('#top_transaction_2').html(html);
+	});
+}
+
+function loadTopTransfer() {
+	$$.getJSON(S_DOMAIN + '/afam/rest/transferTop', {}, function(resp) {
+		if(resp.code < 0) {
+			bridge('window').call('tip', resp.msg || '银证转账服务错误');
+			return;
+		}
+		var html = null;
+		html = Template7.templates.template_top_transfer(resp.data.slice(0, 3));
+		$$('#top_transfer_1').html(html);
+		html = Template7.templates.template_top_transfer(resp.data.slice(3, 6));
+		$$('#top_transfer_2').html(html);
+	});
+}
+
+function initNews() {
 	$$('.infinite-scroll').on('infinite', function () {
-	  // 如果正在加载，则退出
-	  if (loading) return;
-	  // 设置flag
-	  loading = true;
-	  // 模拟1s的加载过程
-	  setTimeout(function () {
-		// 重置加载flag
-		loading = false;
-		if (lastIndex >= maxItems) {
-		  // 加载完毕，则注销无限加载事件，以防不必要的加载
-		  AFA.detachInfiniteScroll($$('.infinite-scroll'));
-		  // 删除加载提示符
-		  $$('.infinite-scroll-preloader').remove();
-		  return;
+		loadNews();
+	});
+}
+
+var loading = false;
+function loadNews() {
+	if (loading) return;
+	loading = true;
+	$$.getJSON(S_DOMAIN + '/afam/rest/news', {"page":1}, function(resp) {
+		var loading = false;
+		if(resp.code < 0) {
+			bridge('window').call('tip', resp.msg || '资讯服务错误');
+			return;
 		}
-		// 生成新条目的HTML
-		var html = '';
-		for (var i = lastIndex + 1; i <= lastIndex + itemsPerLoad; i++) {
-		  html += '<li><a href="#" class="item-link item-content">\
-					  <div class="item-media"><img src="image/info1.jpeg" width="80"></div>\
-						<div class="item-inner">\
-						  <div class="item-title-row">\
-							<div class="item-title">资讯资讯' + i + '</div>\
-						  </div>\
-						  <div class="item-subtitle">Beatles</div>\
-						  <div class="item-text">Lorem ipsum dolor sit amet...</div>\
-						</div></a>\
-			</li>';
+		var itemCount = $$('.list-block li').length;
+		var ITEM_COUNT_MAX = 60;
+		if(itemCount >= ITEM_COUNT_MAX) {
+			$Controller.f7.detachInfiniteScroll($$('.infinite-scroll'));
+			$$('.infinite-scroll-preloader').remove();
+			return;
 		}
-		// 添加新条目
+		var html = null;
+		html = Template7.templates.template_news(resp.data);
 		$$('.list-block ul').append(html);
-		// 更新最后加载的序号
-		lastIndex = $$('.list-block li').length;
-		}, 1000);
 	});
 }
