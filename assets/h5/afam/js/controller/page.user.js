f43b73b117d51771cf79289a111cc62542362294
sovranliu <sovranliu@aliyun.com>
2016-09-05 22:46:50 +0800

    ...

--- assets/h5/afam/js/controller/page.user.js
+++ assets/h5/afam/js/controller/page.user.js
@@ -1,5 +1,45 @@
 // user page controller
 
 $Controller.bind = function() {
-	
+	$Controller.methods.prepare();
 };
+
+$Controller.methods = function() {
+	return {
+		prepare:function() {
+			this.refresh();
+			$$('i.js-refresh').on('click', this.refresh);
+		},
+		refresh:function() {
+			$$.getJSON(S_DOMAIN + '/afam/rest/user/overview', {}, function(resp) {
+				if(resp.code < 0) {
+					if(undefined != resp.msg && resp.msg.length > 0) {
+						bridge('window').call('tip', resp.msg);
+					}
+					return;
+				}
+				var data = resp.data;
+				$$('.user-photo img').attr('src', data.head);
+				$$('p.user-name').html(data.name);
+				$$('.user-time span').html(data.lastLoginTime);
+				var html = '';
+				var j = {};
+				var i = 0;
+				for(var key in data.performances) {
+					j[key] = data.performances[key];
+					if(i >= 3) {
+						html += Template7.templates.template_performance(j);
+						j = {};
+						i = 0;
+					}
+					else {
+						i++;
+						
+					}
+				}
+				$$('.user-data-wrap').html(html);
+				bridge('window').call('tip', '刷新成功');
+			});
+		}
+	};
+}();
