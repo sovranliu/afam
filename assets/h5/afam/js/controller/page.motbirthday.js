215a0425d40069cf7816c72db158e0909cd447fb
sovranliu <sovranliu@aliyun.com>
2016-09-06 13:34:58 +0800

    ...

---
+++ assets/h5/afam/js/controller/page.motbirthday.js
@@ -0,0 +1,35 @@
+// MOT list page controller
+
+$Controller.bind = function() {
+	$Controller.methods.prepare();
+};
+
+$Controller.methods = function() {
+	return {
+		prepare:function() {
+			var _this = this;
+			this.load();
+			$$('#btn_close').on('click',function() {
+				bridge('window').call('close');
+			});
+		},
+		load:function() {
+			$$.getJSON(S_DOMAIN + '/afam/rest/client/mot/list', {}, function(resp) {
+				if(resp.code < 0) {
+					if(undefined != resp.msg && resp.msg.length > 0) {
+						bridge('window').call('tip', resp.msg);
+					}
+					return;
+				}
+				var data = resp.data;
+				var html = Template7.templates.template_birthday(data);
+				$$('.list-block ul').html(html);
+				$$('.list-block i.iconfont').on('click',function() {
+					var im = $$(this).data('im');
+					var name = $$(this).parents('li.accordion-item').find('div.item-title').html();
+					bridge('user').call('chat', name, im);
+				});
+			});
+		}
+	};
+}();
---
+++ assets/h5/afam/js/controller/page.motcategory.js
@@ -0,0 +1,33 @@
+// MOT category page controller
+
+$Controller.bind = function() {
+	$Controller.methods.prepare();
+};
+
+$Controller.methods = function() {
+	return {
+		prepare:function() {
+			var _this = this;
+			this.load();
+			$$('#btn_close').on('click',function(){
+				bridge('window').call('close');
+			});
+		},
+		load:function() {
+			$$.getJSON(S_DOMAIN + '/afam/rest/client/mot/category', {}, function(resp) {
+				if(resp.code < 0) {
+					if(undefined != resp.msg && resp.msg.length > 0) {
+						bridge('window').call('tip', resp.msg);
+					}
+					return;
+				}
+				var data = resp.data;
+				var html = Template7.templates.template_category(data);
+				$$('.list-block ul').html(html);
+				$$('.list-block li').on('click',function(){
+					$$(this).find('.item-after').remove();
+				});
+			});
+		}
+	};
+}();
