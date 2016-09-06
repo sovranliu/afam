4fb6b67593be3a711f5f175f7b9e1c95bf99f169
sovranliu <sovranliu@aliyun.com>
2016-09-06 22:14:58 +0800

    ...

---
+++ assets/h5/afam/js/controller/page.contact.js
@@ -0,0 +1,15 @@
+// contact page controller
+
+$Controller.bind = function() {
+	$Controller.methods.prepare();
+};
+
+$Controller.methods = function() {
+	return {
+		prepare:function() {
+			$$('#btn_close').on('click',function(){
+				bridge('window').call('close');
+			});
+		}
+	};
+}();
